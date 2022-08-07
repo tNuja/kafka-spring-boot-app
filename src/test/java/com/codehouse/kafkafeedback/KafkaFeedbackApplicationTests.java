3e20c16b76e1c7be21a9d673e208359857eb4a1a
tNuja <tanuja.joshi0108@gmail.com>
2022-08-06 20:26:29 -0400

    Boilerplate kafka

---
+++ src/test/java/com/codehouse/kafkafeedback/KafkaFeedbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codehouse.kafkafeedback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaFeedbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
